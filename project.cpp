65651fd3225eea5c9abb2fb01a866724e81bcac2
saugatnp <saugatnp11@gmail.com>
2019-05-19 20:18:02 +0545

    editItemUpdated

--- project.cpp
+++ project.cpp
@@ -37,14 +37,18 @@ void changeToDefaultColor(){
 	changeColor("0f");
 }
 
-
+//to print data centrally alligned
 void print(char s[]){
   printf("\t\t\t%s",s);	
 }
 
+
+
 int main() {
     	
   changeColor("0f");
+  // ascii art
+  printf("\n");
   printf("\t __ ___  __   __  __    _   _  __   _     __   __  __ _   _  __  _    ___    __      __ ___  __ _   _\n");
   printf("\t/__  |  /  | |_/ |_     ||_|| |__| | | | |__| / _ |_  ||_|| |_  | | |  |    /__ |_| /__  |  |_  ||_||\n");
   printf("\t__/  |  |__/ | | |__    |   | |  | | |_| |  | |__||__ |   | |__ | |_|  |    __/  /  __/  |  |__ |   |\n\n");
@@ -60,37 +64,71 @@ int main() {
   switch(choose){
   	case '1':
   		additem();
+  		break;
   	case '2':
-	  edititem();	
+	  edititem();
+	  break;	
 	case '3':
 		display();
+		break;
+	default:
+		clr();
+		main();
   }
 //  clr();
   
 }
+// main menu
+void gotoMenu(){
+		printf("\ndo you want to go back to main menu(y/n)\n");
+	choose=getch();
+	switch(choose){
+		case 'y':
+			clr();
+			main();
+		case 'Y':
+			clr();
+		    main();
+		default:
+		    exit(0);	
+	}
+
+}
+
 //to edit items
 void edititem(){
 	char newn[100];
-	fflush(stdin);
 	printf("enter the name of item to edit");
 	scanf("%s",newn);
-	printf("name to edit is",newn);
-	fp=fopen("D:\\project.txt", "r+b");
+	fflush(stdin);
+	fp=fopen("D:\\project.txt", "a+");
 	rewind(fp);
-	while(fread(&item,sizeof(item),1,fp)){
+	while(fread(&item,sizeof(struct ITEM),1,fp)!=NULL){
+		//printf("%s",strcmp(item.name,newn));
 		//fscanf(fp,"%s%d%d",item.name,item.quantity,item.price);
-		if(strcmp(item.name,newn)==0){
+		if(strcmp(item.name,newn)==1){
+			//ftell(fp);
+			//fseek(fp,0,SEEK_CUR);
 			printf("\nname is %s \n quantity is %d \nrate is %d ",item.name,item.quantity,item.price);
-			print("enter new name");
-			scanf("%s",item.name);
-			fwrite(&item,sizeof(item),1,fp);
-			print("enter new quantity");
-			scanf("%d",item.quantity);
-			fwrite(&item,sizeof(item),1,fp);
-			printf("enter new price");
-			scanf("%d",item.price);
-			fwrite(&item,sizeof(item),1,fp);
+			print("enter new name : ");
+			fflush(stdin);
+            fgets(item.name, sizeof(item.name),stdin);
+			print("enter new quantity : ");
+			scanf("%d",&item.quantity);
+			printf("enter new price : ");
+			scanf("%d",&item.price);
+			ftell(fp);
+			fseek(fp,0,SEEK_CUR);
+			fwrite(&item,sizeof(struct ITEM),1,fp);
+			rewind(fp);
 			fclose(fp);
+			clr();
+			print("Item Succesfully updated ");
+		   	gotoMenu();	
+		}
+		else {
+			printf("the item does not exists");
+			gotoMenu();
 		}
 	}
 }
@@ -113,18 +151,7 @@ void display(){
 		printf("\t%d\t%d",item.quantity,item.price);
 		i++;
 	}
-	printf("\ndo you want to go back to main menu(y/n)\n");
-	choose=getch();
-	switch(choose){
-		case 'y':
-			clr();
-			main();
-		case 'Y':
-			clr();
-		    main();
-		default:
-		    exit(0);	
-	}
+	gotoMenu();
 	fclose(fp);
   	
 }
